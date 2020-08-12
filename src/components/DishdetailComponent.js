a82911be8bcb9025ad89119d608e235ed1c016ba
Joseph <josephvempala@gmail.com>
2020-08-12 20:00:52 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl'
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const required = (val) => val && val.length;
@@ -22,7 +23,7 @@ const required = (val) => val && val.length;
     function RenderDish({dish}) {
             return(
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl+dish.image} alt={dish.name} />
                         <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,11 +1,11 @@
 import * as ActionTypes from './ActionTypes';
-import {DISHES} from '../shared/dishes';
+import {baseUrl} from '../shared/baseUrl';
 
 export const fetchDishes = () => (dispatch) =>{
     dispatch(dishesLoading(true));
-    setTimeout(()=>{
-        dispatch(addDishes(DISHES));
-    }, 2000);
+    return fetch(baseUrl+'dishes')
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)));
 }
 
 export const dishesLoading = () =>({
@@ -22,6 +22,22 @@ export const addDishes = (dishes) => ({
     payload:dishes
 });
 
+export const fetchComments = () => (dispatch) =>{
+    return fetch(baseUrl+'comments')
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)));
+}
+
+export const commentsFailed = (errmess) =>({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload:comments
+});
+
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
@@ -30,4 +46,25 @@ export const addComment = (dishId, rating, author, comment) => ({
         author:author,
         comment:comment
     }
+});
+
+export const fetchPromos = () => (dispatch) =>{
+    dispatch(promosLoading(true));
+    return fetch(baseUrl+'promotions')
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () =>({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) =>({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload:promos
 });
